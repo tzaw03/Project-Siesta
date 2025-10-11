18ae184b1574dd8cf111abda443bd02954b96ea8
Ryan <91402966+rgraves0@users.noreply.github.com>
2025-10-11 22:31:46 +0700

    Update __init__.py

--- bot/helpers/translations/__init__.py
+++ bot/helpers/translations/__init__.py
@@ -1,6 +1,7 @@
 from .tr_en import EN
 from .tr_hi import HI
 from .tr_tr import TR
+from .tr_my import MY # New Import
 #selected
 s = None
-lang_available = [EN, HI, TR]
+lang_available = [EN, HI, TR, MY] # Added MY
