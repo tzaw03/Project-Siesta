a3e076374b2a2a7bb00ad0e7ab88a0ef3d9e08a5
tzaw03 <128713204+tzaw03@users.noreply.github.com>
2025-12-28 23:16:56 +0700

    Update uploder.py

--- bot/helpers/uploder.py
+++ bot/helpers/uploder.py
@@ -1,19 +1,14 @@
 import os
-
-from ..settings import bot_set
+import shutil
+from config import Config
 from .message import send_message, edit_message
 from .utils import *
-
-#
-#
-#  TASK HANDLER
-#
-#
 
 async def track_upload(metadata, user, disable_link=False):
     if bot_set.upload_mode == 'Local':
         await local_upload(metadata, user)
     elif bot_set.upload_mode == 'Telegram':
+        # send_message က Channel ထဲကိုပါ အလိုအလျောက် dump လုပ်သွားပါလိမ့်မယ်
         await telegram_upload(metadata, user)
     else:
         rclone_link, index_link = await rclone_upload(user, metadata['filepath'])
@@ -21,11 +16,10 @@ async def track_upload(metadata, user, disable_link=False):
             await post_simple_message(user, metadata, rclone_link, index_link)
 
     try:
-        os.remove(metadata['filepath'])
-    except FileNotFoundError:
-        pass
-        
-
+        if os.path.exists(metadata['filepath']):
+            os.remove(metadata['filepath'])
+    except Exception as e:
+        LOGGER.error(f"Cleanup Error: {e}")
 
 async def album_upload(metadata, user):
     if bot_set.upload_mode == 'Local':
@@ -33,9 +27,7 @@ async def album_upload(metadata, user):
     elif bot_set.upload_mode == 'Telegram':
         if bot_set.album_zip:
             for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
+                await send_message(user, item, 'doc', caption=await create_simple_text(metadata, user))
         else:
             await batch_telegram_upload(metadata, user)
     else:
@@ -47,140 +39,44 @@ async def album_upload(metadata, user):
                 pass
         else:
             await post_simple_message(user, metadata, rclone_link, index_link)
-
     await cleanup(None, metadata)
 
-
-async def artist_upload(metadata, user):
-    if bot_set.upload_mode == 'Local':
-        await local_upload(metadata, user)
-    elif bot_set.upload_mode == 'Telegram':
-        if bot_set.artist_zip:
-            for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
-        else:
-            pass # artist telegram uploads are handled by album fucntion
-    else:
-        rclone_link, index_link = await rclone_upload(user, metadata['folderpath'])
-        if metadata['poster_msg']:
-            try:
-                await edit_art_poster(metadata, user, rclone_link, index_link, await format_string(lang.s.ARTIST_TEMPLATE, metadata, user))
-            except MessageNotModified:
-                pass
-        else:
-            await post_simple_message(user, metadata, rclone_link, index_link)
-
-    await cleanup(None, metadata)
-
-
-
-async def playlist_upload(metadata, user):
-    if bot_set.upload_mode == 'Local':
-        await local_upload(metadata, user)
-    elif bot_set.upload_mode == 'Telegram':
-        if bot_set.playlist_zip:
-            for item in metadata['folderpath']:
-                await send_message(user,item,'doc', 
-                    caption=await create_simple_text(metadata, user)
-                )
-        else:
-            await batch_telegram_upload(metadata, user)
-    else:
-        if bot_set.playlist_sort and not bot_set.playlist_zip:
-            if bot_set.disable_sort_link:
-                await rclone_upload(user, f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/")
-            else:
-                for track in metadata['tracks']:
-                    try:
-                        rclone_link, index_link = await rclone_upload(user, track['filepath'])
-                        if not bot_set.disable_sort_link:
-                            await post_simple_message(user, track, rclone_link, index_link)
-                    except ValueError: # might try to upload track which is not available
-                        pass
-        else:
-            rclone_link, index_link = await rclone_upload(user, metadata['folderpath'])
-            if metadata['poster_msg']:
-                try:
-                    await edit_art_poster(metadata, user, rclone_link, index_link, await format_string(lang.s.PLAYLIST_TEMPLATE, metadata, user))
-                except MessageNotModified:
-                    pass
-            else:
-                await post_simple_message(user, metadata, rclone_link, index_link)
-
-#
-#
-#  CORE
-#
-#
-
-async def rclone_upload(user, realpath):
-    """
-    Args:
-        user: user details
-        realpath: full path to (not used for uploading)
-    Returns:
-        rclone_link, index_link
-    """
-    path = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/"
-    cmd = f'rclone copy --config ./rclone.conf "{path}" "{Config.RCLONE_DEST}"'
-    task = await asyncio.create_subprocess_shell(cmd)
-    await task.wait()
-    r_link, i_link = await create_link(realpath, Config.DOWNLOAD_BASE_DIR + f"/{user['r_id']}/")
-    return r_link, i_link
-
-
-async def local_upload(metadata, user):
-    """
-    Copies directory to local storage and merges contents if the destination exists.
-    Args:
-        metadata: metadata dict of item
-        user: user details
-    """
-    to_move = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/{metadata['provider']}"
-    destination = os.path.join(Config.LOCAL_STORAGE, os.path.basename(to_move))
-
-    # If the destination directory exists, merge contents
-    if os.path.exists(destination):
-        for item in os.listdir(to_move):
-            src_item = os.path.join(to_move, item)
-            dest_item = os.path.join(destination, item)
-
-            # If it's a file, copy it; if it's a directory, use copytree
-            if os.path.isdir(src_item):
-                if not os.path.exists(dest_item):
-                    shutil.copytree(src_item, dest_item)
-            else:
-                shutil.copy2(src_item, dest_item)
-    else:
-        shutil.copytree(to_move, destination)
-    
-    shutil.rmtree(to_move)
-
+# Artist နဲ့ Playlist upload တွေဟာလည်း telegram_upload/send_message ကိုပဲ သုံးတာမို့
+# Channel Dump Feature က အလိုအလျောက် အလုပ်လုပ်ပါလိမ့်မယ်။
 
 async def telegram_upload(track, user):
-    """
-    Only upload a single track
-    Args:
-        track: track metadata
-        """
+    """Only upload a single track"""
     await send_message(user, track['filepath'], 'audio', meta=track)
 
-
 async def batch_telegram_upload(metadata, user):
-    """
-    Args:
-        metadata: full metadata
-        user: user details
-    """
-    if metadata['type'] == 'album' or metadata['type'] == 'playlist':
+    if metadata['type'] in ['album', 'playlist']:
         for track in metadata['tracks']:
             try:
                 await telegram_upload(track, user)
-            except FileNotFoundError:
+            except:
                 pass
     elif metadata['type'] == 'artist':
         for album in metadata['albums']:
             for track in album['tracks']:
                 await telegram_upload(track, user)
+
+async def local_upload(metadata, user):
+    to_move = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/{metadata['provider']}"
+    destination = os.path.join(Config.LOCAL_STORAGE, os.path.basename(to_move))
+    if os.path.exists(destination):
+        for item in os.listdir(to_move):
+            src_item = os.path.join(to_move, item)
+            dest_item = os.path.join(destination, item)
+            if os.path.isdir(src_item):
+                if not os.path.exists(dest_item): shutil.copytree(src_item, dest_item)
+            else: shutil.copy2(src_item, dest_item)
+    else:
+        shutil.copytree(to_move, destination)
+    shutil.rmtree(to_move)
+
+async def rclone_upload(user, realpath):
+    path = f"{Config.DOWNLOAD_BASE_DIR}/{user['r_id']}/"
+    cmd = f'rclone copy --config ./rclone.conf "{path}" "{Config.RCLONE_DEST}"'
+    task = await asyncio.create_subprocess_shell(cmd)
+    await task.wait()
+    return await create_link(realpath, Config.DOWNLOAD_BASE_DIR + f"/{user['r_id']}/")
